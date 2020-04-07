3e2d1b55c50abeddf427e8bf46a320ed4f0f385a
Rin Cat <me@rin.cat>
2020-04-07 16:59:22 -0400

    Delete duplicate

--- include/ieee80211.h
+++ include/ieee80211.h
@@ -1627,7 +1627,7 @@ static __always_inline int is_broadcast_mac_addr(const u8 *addr)
 		(addr[3] == 0xff) && (addr[4] == 0xff) && (addr[5] == 0xff));
 }
 
-static __always_inline __inline int is_zero_mac_addr(const u8 *addr)
+static __always_inline int is_zero_mac_addr(const u8 *addr)
 {
 	return ((addr[0] == 0x00) && (addr[1] == 0x00) && (addr[2] == 0x00) &&   \
 		(addr[3] == 0x00) && (addr[4] == 0x00) && (addr[5] == 0x00));

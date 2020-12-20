7accfe2b6480a4005c761080e5ffc3744d9234cb
Guilherme Alcarde Gallo <gagallo7@gmail.com>
2020-12-20 17:52:51 -0300

    Adapt cfg80211 rtw_cfg80211_ch_switch_notify ioctl
    * This ioctl signature was changed in kernel 5.10.1
    * It has a new parameter: the quiet boolean. I guessed that this
    value should be true in this case.

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -442,7 +442,11 @@ u8 rtw_cfg80211_ch_switch_notify(_adapter *adapter, u8 ch, u8 bw, u8 offset,
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 19, 0))
 	if (started) {
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0))
+		cfg80211_ch_switch_started_notify(adapter->pnetdev, &chdef, 0, true);
+	#else
 		cfg80211_ch_switch_started_notify(adapter->pnetdev, &chdef, 0);
+	#endif
 		goto exit;
 	}
 #endif

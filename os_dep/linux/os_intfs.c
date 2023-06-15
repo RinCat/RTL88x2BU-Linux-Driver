7bb4c9ca69277f396d92557578776926625411db
Cannonball7 <james@nineseven.net>
2023-06-14 19:44:06 -0700

    `Wifi Protected Setup - Push Button Configuration` is not related to `Universal Serial Bus - Host Controller Interface`

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -427,11 +427,17 @@ int rtw_hwpwrp_detect = 1;
 int rtw_hwpwrp_detect = 0; /* HW power  ping detect 0:disable , 1:enable */
 #endif
 
-#ifdef CONFIG_USB_HCI
-int rtw_hw_wps_pbc = 1;
-#else
+
+/*
+Causes excessive rescanning by nature of WPS, the "Push Button Configuration"
+	being something initialized on the access point by button due to the 
+	traditionally non-graphical nature of routers. This should only be enabled
+	during those events, otherwise the radio(s) will have to drop beam forming
+	capabilities to do a general scan for an access point emitting the unsecured
+	connection information.
+*/
 int rtw_hw_wps_pbc = 0;
-#endif
+
 
 #ifdef CONFIG_80211D
 int rtw_80211d = 0;

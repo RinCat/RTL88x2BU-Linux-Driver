14a60a73997dbde204ec1ff39f9c38f15c1b9a63
Alex Shumsky <alexthreed@gmail.com>
2024-06-07 23:47:17 +0300

    fix netdev (un)registration deadlock

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -2192,7 +2192,11 @@ int rtw_os_ndev_register(_adapter *adapter, const char *name)
 	if (rtnl_lock_needed)
 		ret = (register_netdev(ndev) == 0) ? _SUCCESS : _FAIL;
 	else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0)
+		ret = (cfg80211_register_netdevice(ndev) == 0) ? _SUCCESS : _FAIL;
+#else
 		ret = (register_netdevice(ndev) == 0) ? _SUCCESS : _FAIL;
+#endif
 
 	if (ret == _SUCCESS)
 		adapter->registered = 1;
@@ -2241,7 +2245,11 @@ void rtw_os_ndev_unregister(_adapter *adapter)
 		if (rtnl_lock_needed)
 			unregister_netdev(netdev);
 		else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0)
+			cfg80211_unregister_netdevice(netdev);
+#else
 			unregister_netdevice(netdev);
+#endif
 	}
 
 #if defined(CONFIG_IOCTL_CFG80211) && !defined(RTW_SINGLE_WIPHY)
